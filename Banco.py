c8891482a93a31e48f3acc2a5e586fbd37d09125
Luiz <lvj253@gmail.com>
2022-07-10 18:11:50 -0300

    commit final

--- Banco.py
+++ Banco.py
@@ -31,16 +31,64 @@ class Command(ABC):
         raise NotImplementedError("Necessário implementar")
 
 class StartLoop(Command):
-    def execute(self, wn):
+    name_var =None
+    amount_var =None
+    costumer = None
+
+    def submit_transacao(self):
+        name = self.name_var.get()
+        amount = self.amount_var.get()
+
+        self.costumer.balance = int(self.costumer.balance) - int(amount)
+        self.costumer.extratos.append(Transacao(int(amount), self.costumer.name, name))
+
+        self.name_var.set("")
+        self.amount_var.set("")
+    
+
+    def show_transacoes(self):
+        for transacao in self.costumer.extratos:
+            if(transacao.nameReceiver == self.costumer.name):
+                tk.Label(text="Recebido " + str(transacao.amount) + " de " + transacao.nameSender).pack()
+            else:
+                tk.Label(text="Enviado " + str(transacao.amount) + " para " + transacao.nameReceiver).pack()
+
+
+    def execute(self, wn, cliente):
+        self.name_var = tk.StringVar()
+        self.amount_var = tk.StringVar()
+        self.costumer = cliente
+        tk.Label(text="                             Hello, " + cliente.name + "                       \n").pack()
+        tk.Label(text = "Seu saldo: " + str(cliente.balance) + "\n").pack()
+        tk.Label(text="Transferir para: ").pack()
+        entry = tk.Entry(width = 20, textvariable=self.name_var)
+        entry.focus_set()
+        entry.pack()
+
+        tk.Label(text="Quantidade a transferir: ").pack()
+        entry = tk.Entry(width = 20, textvariable=self.amount_var)
+        entry.focus_set()
+        entry.pack()
+        tk.Button(text="Transferir", command= lambda : {self.submit_transacao(self)}).pack()
+
+        tk.Button(text="Ver extrato", command= lambda: {self.show_transacoes(self)}).pack()
+
         wn.mainloop()
+
         
-
-
+        
 
 class Invoker:
 
     window = tk.Tk()
 
-    def set_on_start(self, command):
+    def set_on_start(self, command, cliente):
         self._on_start = command
+        self._on_start.execute(self._on_start, self.window, cliente)
+
+# ------ Testes ------------
+
+invoker = Invoker()
+cliente = Cliente(1, "Luiz", 500)
+invoker.set_on_start(StartLoop, cliente)
 
